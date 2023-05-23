6706e0819865e7afd6e5f6ba6ea9016e99e75189
lucklyYong <luckyzangvip@163.com>
2023-05-23 09:29:22 +0800

    删除测试注释

--- src/pages/BeaconList.vue
+++ src/pages/BeaconList.vue
@@ -200,9 +200,9 @@ export default {
         }
       }, 10000);
       // 轮询设备蓝牙是否打开
-      // this.windowTimer = window.setInterval(() => {
-      //   setTimeout(this.judgeIsOpenBluetooth(), 0)
-      // }, 2000);
+      this.windowTimer = window.setInterval(() => {
+        setTimeout(this.judgeIsOpenBluetooth(), 0)
+      }, 2000);
       this.changeGlobalTimer(this.windowTimer)
     }
   },

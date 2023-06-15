0879ceaec69bf1862c3ea4b121057f394c422acb
lucklyYong <luckyzangvip@163.com>
2023-06-15 10:10:51 +0800

    下拉搜索组件完善

--- src/components/SelectSearch.vue
+++ src/components/SelectSearch.vue
@@ -74,7 +74,7 @@ import { deepClone } from '@/common/js/utils'
 			this.datalist = this.itemData;
 		   //单选
 		   if (!this.multiple) {
-			 if (newVal == null || !isNaN(newVal)) {
+			 if (newVal == null || (!isNaN(newVal) && typeof Number(newVal) === 'number')) {
 				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0]['text'] : '';
 		   	 	this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == newVal})[0] : null
 			 } else {
@@ -138,7 +138,7 @@ import { deepClone } from '@/common/js/utils'
 		this.datalist = this.itemData;
 		//单选
 		if (!this.multiple) {
-			if (this.curData == null || !isNaN(this.curData )) {
+			if (this.curData == null || (!isNaN(this.curData) && typeof Number(this.curData) === 'number')) {
 				this.current = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData})[0]['text'] : '';
 				this.currentFullValue = this.datalist.length > 0 ? this.datalist.filter((item) => { return item.value == this.curData})[0] : null
 			} else {
